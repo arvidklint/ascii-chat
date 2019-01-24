06c5884fe3c3483115e0b3a40f6f43b2fcb27722
Arvid Sätterkvist <arvidsat@gmail.com>
2019-01-24 21:38:37 +0100

    Add better build and dev environment

--- server/index.js
+++ server/index.js
@@ -3,15 +3,20 @@ const path = require('path');
 
 const app = new express();
 
-const root = path.resolve(__dirname, '../public');
+const options = {
+    dotfiles: 'ignore',
+    etag: false,
+    maxAge: '1d',
+    redirect: false,
+    setHeaders: function (res, path, stat) {
+        res.set('x-timestamp', Date.now());
+    }
+}
 
-app.use(express.static(root));
+app.use(express.static(path.resolve(__dirname, 'public'), options));
 
 app.use('*', function(req, res) {
-    res.sendFile(
-        'index.html',
-        { root }
-    );
+    res.sendFile(path.resolve(__dirname, './public', 'index.html'));
 });
 
 app.listen("8080");
